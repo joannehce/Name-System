1632116fe313e9b82c39623f5de0126e788224e5
Chia En Hsu <Joanne@chiatekimbp.attlocal.net>
2018-05-28 19:34:27 -0500

    Add new file

---
+++ 0528namesystem.py
@@ -0,0 +1,13 @@
+#名字管理器
+#打印功能
+print("="*50)
+print('Name System')
+print('1: add new name')
+print('2: delet name')
+print('3: revise name')
+print('4: search name')
+print('5: quite')
+print("="*50)
+name = []
+#獲取用戶的選擇
+#根據選擇，執行相印功能
